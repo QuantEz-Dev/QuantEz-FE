b330197c38318742c58c1fec3c917a45fe04812f
kwakseongjae <gkffhdnls13@gmail.com>
2023-09-12 16:26:57 +0900

    FEAT: 기존 프로젝트 파일 업로드

---
+++ src/subpages/FinanceSheet/FinanceSheet-trade1.js
@@ -0,0 +1,19 @@
+import { Content1 } from "../../components/Content1/Content1";
+import { Sheets } from "../../components/Sheets";
+
+
+export const FinanceSheet1 = () => {
+  return (
+    <div className="container">
+      <ariticle>
+        <section>
+          <div className="content">
+            <Sheets />
+            <Content1 />
+            
+          </div>
+        </section>
+      </ariticle>
+    </div>
+  );
+};
