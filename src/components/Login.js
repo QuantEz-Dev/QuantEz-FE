23b6bb64a4971077d5d998550f8309bed540b070
bigmooon <bigmooon1215@gmail.com>
2023-09-24 08:27:42 +0900

    ADD:Login.js 수정

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,28 +3,69 @@ import * as S from "../styles/Login.styled";
 
 export const Login = () => {
   const [signIn, toggle] = useState(true);
-  // const [usernameReg, setUsernameReg] = useState("");
-  // const [passReg, setPasswordReg] = useState("");
-  // const [userEmailReg, setUserEmailReg] = useState("");
+  const [usernameReg, setUsernameReg] = useState("");
+  const [passReg, setPasswordReg] = useState("");
+  const [userEmailReg, setUserEmailReg] = useState("");
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const user = {
+      username: setUsernameReg,
+      email: setUserEmailReg,
+      password: setPasswordReg
+    };
+
+    fetch("http://127.0.0.1:8000/api/", {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json)
+      .then(data => {
+        if (data.key) {
+          localStorage.clear();
+          localStorage.setItem('token', data.key);
+          window.location.replace('http://localhost:3000/login');
+      } else {
+        setUsernameReg('');
+        setUserEmailReg('');
+        setPasswordReg('');
+        localStorage.clear();
+        }
+      });
+  };
+
+  const onChange = e => {
+    setUsernameReg(e.target.value);
+    setUserEmailReg(e.target.value);
+    setPasswordReg(e.target.value);
+  };
 
   return (
     <S.Wrap>
       <S.Container>
         <S.SignUpContainer signinIn={signIn}>
-          <S.Form>
+          <S.Form onSubmit={onSubmit}>
             <S.Title>Create Account</S.Title>
-            <S.Input name="name" type="text" placeholder="Name" required="" />
+            <S.Input name="name" type="text" value={setUsernameReg} placeholder="Name" required onChange={e => setUsernameReg(e.target.value)}/>
             <S.Input
               name="email"
               type="email"
+              value={setUserEmailReg}
               placeholder="Email"
-              required=""
+              required
+              onChange={e => setUserEmailReg(e.target.value)}
             />
             <S.Input
               name="password"
               type="password"
               placeholder="Password"
-              required=""
+              value={setPasswordReg}
+              required
+              onChange={e => setPasswordReg(e.target.value)}
             />
             <S.Button type="submit">Sign Up</S.Button>
           </S.Form>
@@ -33,8 +74,8 @@ export const Login = () => {
         <S.SignInContainer signinIn={signIn}>
           <S.Form>
             <S.Title>Sign in</S.Title>
-            <S.Input name="email" type="email" placeholder="Email" />
-            <S.Input name="password" type="password" placeholder="Password" />
+            <S.Input name="email" type="email" placeholder="Email" required />
+            <S.Input name="password" type="password" placeholder="Password" required />
             <S.Anchor href="#">Forgot your password?</S.Anchor>
             <S.Button type="submit">Sign In</S.Button>
           </S.Form>
